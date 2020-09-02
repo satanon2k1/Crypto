91a35c392c6f82e9a0f5e0ca5ced029785d66dab
Bùi Trung Quân <44649106+satanon2k1@users.noreply.github.com>
2020-09-03 02:00:47 +0700

    Create WienerRSA.py

---
+++ RSA/Wiener/WienerRSA.py
@@ -0,0 +1,21 @@
+'''
+require sage installed
+'''
+
+from sage.all import *
+
+def wiener(e, n):
+	cf = continued_fraction(e / n)
+	conv = cf.convergents()
+	q0 = 1
+	c = pow(8101, e, n)
+	for i in conv:
+		q1 = int(i.denominator())
+		if pow(c, q1, n) == 8101:
+			return q1
+		else:
+			for r in range(30) for s in range(30):
+				d = r * q1 + s * q0
+				if pow(c, d, n) == 8101:
+					return d
+			q0 = q1
