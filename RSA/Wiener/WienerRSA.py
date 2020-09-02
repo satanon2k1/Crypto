53b9aab82860e587b48e7dfd29af76a2048d7348
Bùi Trung Quân <44649106+satanon2k1@users.noreply.github.com>
2020-09-03 02:47:57 +0700

    Update WienerRSA.py

--- RSA/Wiener/WienerRSA.py
+++ RSA/Wiener/WienerRSA.py
@@ -1,16 +1,34 @@
-'''
-require sage installed
-'''
+from fractions import Fraction
 
-from sage.all import *
+def ContinuedFraction(e, n):
+	result = []
+	while True:
+		q, r = n // e, n % e
+		result.append(q)
+		if r == 0:
+			return result
+		n, e = e, r
+
+def Recursive(cf, first, i):
+	if first <= i:
+		return Fraction(1, cf[first] + Recursive(cf, first + 1, i))
+	else:
+		return 0
+
+def Convergents(cf):
+	result = []
+	for i in range(len(cf)):
+		node = Recursive(cf, 0, i)
+		result.append(node)
+	return result
 
 def main(e, n):
-	cf = continued_fraction(e / n)
-	conv = cf.convergents()
+	cf = ContinuedFraction(e, n)
+	conv = Convergents(cf)
 	q0 = 1
 	c = pow(8101, e, n)
 	for i in conv:
-		q1 = int(i.denominator())
+		q1 = i.denominator
 		if pow(c, q1, n) == 8101:
 			return q1
 		else:
@@ -20,3 +38,5 @@ def main(e, n):
 					if pow(c, d, n) == 8101:
 						return d
 			q0 = q1
+
+print(main(17993, 90581))
