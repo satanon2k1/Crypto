0545b56dd78693028855c8ad4169ce51f1d2bf2c
satanon2k1 <buitrungquan812001@gmail.com>
2020-10-11 00:22:50 +0700

    Fix code

--- others/GramSchmidt.py
+++ others/GramSchmidt.py
@@ -32,21 +32,21 @@ def Size(u):
 
 	return size
 
-V = [
-[4,1,3,-1],
-[2,1,-3,4],
-[1,0,-2,7],
-[6,2,9,-5]
-]
+# V = [
+# [4,1,3,-1],
+# [2,1,-3,4],
+# [1,0,-2,7],
+# [6,2,9,-5]
+# ]
 
-U = [V[0]]
+# U = [V[0]]
 
-for i in range(1, len(V)):
-	ui = V[i]
-	for j in range(i):
-		uij = ScalaMulti(V[i], U[j]) / Size(U[j])
-		ui = SubtractVector(ui, DemensionalMulti(uij, U[j]))
+# for i in range(1, len(V)):
+# 	ui = V[i]
+# 	for j in range(i):
+# 		uij = ScalaMulti(V[i], U[j]) / Size(U[j])
+# 		ui = SubtractVector(ui, DemensionalMulti(uij, U[j]))
 
-	U.append(ui)
+# 	U.append(ui)
 
-print(U)
+# print(U)
